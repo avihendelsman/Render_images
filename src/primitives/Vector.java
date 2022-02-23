45cdd58d07b2c30699cddac07c6019cd3f412b38
avihendelsman <avihndlsmann@gmail.com>
2022-02-23 16:25:20 +0200

    16:25

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -20,13 +20,15 @@ public class Vector extends Point {
         return new Vector(temp.d1, temp.d2, temp.d3);
     }
 
-    public double dotProduct(Vector V2){
-        Double3 temp = xyz.product(V2.xyz);
+    public double dotProduct(Vector v2){
+        Double3 temp = xyz.product(v2.xyz);
         return temp.d1+ temp.d2+ temp.d3 ;
     }
 
-    public Vector crossProduct(){
-        return ;
-
+    public Vector crossProduct(Vector v2){
+        Double3 temp = new Double3((xyz.d2 * v2.xyz.d3) - (xyz.d3 * v2.xyz.d2),
+                - ((xyz.d1 * v2.xyz.d3) - (xyz.d3 * v2.xyz.d1)),
+                (xyz.d1 * v2.xyz.d2) - (xyz.d2 * v2.xyz.d1));
+        return new Vector(temp.d1, temp.d2, temp.d3);
     }
 }
