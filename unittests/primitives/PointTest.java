5d8c10ba19097b41adf582ff42e6bbf45a270269
osher-v <oshervaza1@gmail.com>
2022-03-01 20:31:19 +0200

    20:30

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,51 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit tests for primitives.Point class
+ */
+
+class PointTest {
+    /**
+     * Test method for subtract function.
+     */
+    @Test
+    void testSubtract() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
+    }
+    /**
+     * Test method for add function.
+     */
+    @Test
+    void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
+    }
+    /**
+     * Test method for distanceSquared function.
+     */
+    @Test
+    void testDistanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
+    }
+    /**
+     * Test method for distance function.
+     */
+    @Test
+    void testDistance() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
+    }
+}
