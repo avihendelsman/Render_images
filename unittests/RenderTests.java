c6085588db79a1937305d312ef378bf4a525cd21
osher-v <oshervaza1@gmail.com>
2022-04-05 11:15:26 +0300

    final ex5

--- unittests/RenderTests.java
+++ unittests/RenderTests.java
@@ -16,6 +16,7 @@ public class RenderTests {
 	/**
 	 * Produce a scene with basic 3D model and render it into a png image with a
 	 * grid
+	 *
 	 */
 	@Test
 	public void basicRenderTwoColorTest() {
